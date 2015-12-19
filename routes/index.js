a3b64b88bbf7274cf5caa40c9d6e7625b731861c
ParkJunHyuk <akdltjswnsgu@naver.com>
2015-12-20 06:03:01 +0900

    공식스케줄까지

--- routes/index.js
+++ routes/index.js
@@ -62,6 +62,71 @@ router.get('/scheduleReq', function (req, res, next) {
     }
 });
 
+router.get('/scheduleAptA', function (req, res, next) {
+    if (req.session.user) {
+        var sql = "select * from personal_schedule where accept_op = 'A'";
+        pool.getConnection(function (err, conn) {
+            conn.query(sql, function (err, rows) {
+                res.render('page/scheduleApt', {
+                    title: '스케줄승인현황',
+                    rows: rows,
+                    name: req.session.user[0].admin_name
+                });
+            });
+            conn.release();
+        });
+    } else {
+        res.render('login');
+    }
+});
+
+router.get('/scheduleAptR', function (req, res, next) {
+    if (req.session.user) {
+        var sql = "select * from personal_schedule where accept_op = 'R'";
+        pool.getConnection(function (err, conn) {
+            conn.query(sql, function (err, rows) {
+                res.render('page/scheduleApt', {
+                    title: '스케줄거절현황',
+                    rows: rows,
+                    name: req.session.user[0].admin_name
+                });
+            });
+            conn.release();
+        });
+    } else {
+        res.render('login');
+    }
+});
+
+router.get('/offScheduleAdd', function (req, res, next) {
+    if (req.session.user) {
+        res.render('page/offScheduleAdd', {
+            title: '스케줄거절현황',
+            name: req.session.user[0].admin_name
+        });
+    } else {
+        res.render('login');
+    }
+});
+
+router.get('/offScheduleList', function (req, res, next) {
+    if (req.session.user) {
+        var sql = "select * from official_schedule o, admin a where o.admin_num = a.admin_num";
+        pool.getConnection(function (err, conn) {
+            conn.query(sql, function (err, rows) {
+                res.render('page/offScheduleList', {
+                    title: '공식스케줄현황',
+                    rows: rows,
+                    name: req.session.user[0].admin_name
+                });
+            });
+            conn.release();
+        });
+    } else {
+        res.render('login');
+    }
+});
+
 
 //
 //router.get('/index', function(req, res, next) {
