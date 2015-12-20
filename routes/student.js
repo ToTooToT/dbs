343c10953c3c4e0385a8ae62c76bfe23206ecb93
ParkJunHyuk <akdltjswnsgu@naver.com>
2015-12-20 20:31:59 +0900

    학생수정완료

--- routes/student.js
+++ routes/student.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 
 router.get('/', function (req, res, next) {
-    pool.getConnection(function (err, conn) {
-        conn.query("select N.script_num as nid, N.title as title, N.reg_date as reg_date, A.admin_name as writer from notice N, admin A where N.admin_num=A.admin_num  order by reg_date desc", function (err, rows) {
-            if (err) console.log("err:" + err);
-            console.log(JSON.stringify(rows));
-            res.render('page/noticeList', {
-                title: '공지사항',
-                rows: rows,
-                name: req.session.user[0].admin_name
+    if (req.session.user) {
+        pool.getConnection(function (err, conn) {
+            conn.query("select *, admin_name as writer, script_num as nid from notice N, admin A where N.admin_num = A.admin_num order by reg_date desc", function (err, rows) {
+                if (err) console.log("err:" + err);
+                console.log(JSON.stringify(rows));
+                res.render('page/noticeList', {
+                    title: '공지사항',
+                    rows: rows,
+                    name: req.session.user[0].s_name,
+                    mode: req.session.grade
+                });
             });
+            conn.release();
         });
-        conn.release();
-    });
+    } else {
+        res.render('login');
+    }
 });
 
 router.get('/notice/:nid/:writer', function (req, res, next) {
@@ -29,7 +34,8 @@ router.get('/notice/:nid/:writer', function (req, res, next) {
                     title: '공지보기',
                     row: row,
                     writer: writer,
-                    name: req.session.user[0].admin_name
+                    name: req.session.user[0].s_name,
+                    mode: req.session.grade
                 });
             });
             conn.release();
@@ -41,44 +47,56 @@ router.get('/notice/:nid/:writer', function (req, res, next) {
 
 
 router.get('/timeInsert', function (req, res, next) {
+    if (req.session.user) {
 
-    res.render('page/timeInsert', {
-        title: '수업 시간등록',
-        name: req.session.user[0].admin_name,
-        mode: req.session.grade
-    });
-
+        res.render('page/timeInsert', {
+            title: '수업 시간등록',
+            name: req.session.user[0].s_name,
+            mode: req.session.grade
+        });
+    } else {
+        res.render('login');
+    }
 });
 router.post('/timeInsert2', function (req, res, next) {
+    if (req.session.user) {
 
-    res.render('page/timeInsert2', {
-        title: '상주 시간등록',
-        name: req.session.user[0].admin_name,
-        mode: req.session.grade
-    });
-
+        res.render('page/timeInsert2', {
+            title: '상주 시간등록',
+            name: req.session.user[0].s_name,
+            mode: req.session.grade
+        });
+    } else {
+        res.render('login');
+    }
 });
 
 
 router.get('/scheduleInsert', function (req, res, next) {
+    if (req.session.user) {
 
-    res.render('page/scheduleInsert', {
-        title: '개인스케줄등록',
-        name: req.session.user[0].admin_name,
-        mode: req.session.grade
-    });
-
+        res.render('page/scheduleInsert', {
+            title: '개인스케줄등록',
+            name: req.session.user[0].s_name,
+            mode: req.session.grade
+        });
+    } else {
+        res.render('login');
+    }
 });
 
 
 router.get('/schedulePresent', function (req, res, next) {
+    if (req.session.user) {
 
-    res.render('page/schedulePresent', {
-        title: '개인스케줄현황',
-        name: req.session.user[0].admin_name,
-        mode: req.session.grade
-    });
-
+        res.render('page/schedulePresent', {
+            title: '개인스케줄현황',
+            name: req.session.user[0].s_name,
+            mode: req.session.grade
+        });
+    } else {
+        res.render('login');
+    }
 });
 
 
